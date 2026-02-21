b07f954876b489e6d5d7e1d6385af6b8a5d4ecd2
9SAN <omentiumhybrid@gmail.com>
2026-02-21 07:44:27 +0700

    Update hamiltonian_hubbard.py

--- src/hamiltonian_hubbard.py
+++ src/hamiltonian_hubbard.py
@@ -1,1 +1,10 @@
+import numpy as np
 
+
+def build_hubbard_hamiltonian(t=1.0, U=4.0, epsilon=0.0, V=0.0):
+    return {
+        "t": t,
+        "U": U,
+        "epsilon": epsilon,
+        "V": V
+    }
