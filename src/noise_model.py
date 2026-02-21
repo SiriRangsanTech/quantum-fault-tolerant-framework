a628ea2026616e600454349b641a3e5961955706
9SAN <omentiumhybrid@gmail.com>
2026-02-21 07:43:57 +0700

    Update noise_model.py

--- src/noise_model.py
+++ src/noise_model.py
@@ -1,1 +1,12 @@
+import cirq
 
+
+def build_noise_model(T=0.1):
+    gamma = T**5
+
+    noise_ops = [
+        cirq.amplitude_damp(gamma),
+        cirq.phase_damp(gamma)
+    ]
+
+    return cirq.NoiseModel.from_noise_ops(noise_ops)
