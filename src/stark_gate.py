c145b000c7d155646ed81b8dc1ba4939e8f4fd0c
9SAN <omentiumhybrid@gmail.com>
2026-02-21 07:43:29 +0700

    Update stark_gate.py

--- src/stark_gate.py
+++ src/stark_gate.py
@@ -1,1 +1,21 @@
+import cirq
+import numpy as np
 
+
+class StarkGate(cirq.Gate):
+    def __init__(self, alpha1=0.1, alpha2=0.01, voltage=1.0, duration=1.0):
+        self.alpha1 = alpha1
+        self.alpha2 = alpha2
+        self.voltage = voltage
+        self.duration = duration
+
+    def _unitary_(self):
+        E = self.voltage
+        phi = (self.alpha1 * E + self.alpha2 * E**2) * self.duration
+        return cirq.unitary(cirq.rz(phi))
+
+    def num_qubits(self):
+        return 1
+
+    def __str__(self):
+        return f"StarkGate(V={self.voltage})"
